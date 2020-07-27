363beb5c8890d5b59fdfeaf3ff09d80e9f4573cb
vannnyle@gmail.com <vannnyle@gmail.com>
2020-07-27 22:28:54 +0300

    Build the recipe view

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
 import Search from './models/Search';
 import Recipe from "./models/Recipe";
 import * as searchView from './views/searchView';
+import * as recipeView from './views/recipeView';
 import {elements, renderLoader, clearLoader} from "./views/base";
 /*
 Global state of the app:
@@ -62,7 +63,8 @@ const controlRecipe = async () => {
 
     if(id) {
         // Prepare UI for changes
-
+        recipeView.clearRecipe();
+        renderLoader(elements.recipe);
         // Create new recipe object
         state.recipe = new Recipe(id);
 
@@ -76,7 +78,8 @@ const controlRecipe = async () => {
             state.recipe.calcServing();
             state.recipe.calcTime();
             // Render recipe
-            console.log(state.recipe);
+            clearLoader();
+            recipeView.renderRecipe(state.recipe);
         } catch (error) {
             alert(`Error processing recipe!`)
         }
