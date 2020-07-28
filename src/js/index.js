7722cc5b32ae9885e37291b43fa1fbafeb65dfea
vannnyle@gmail.com <vannnyle@gmail.com>
2020-07-28 22:32:31 +0300

    Build the shopping list model

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,6 @@
 import Search from './models/Search';
 import Recipe from "./models/Recipe";
+import List from "./models/List";
 import * as searchView from './views/searchView';
 import * as recipeView from './views/recipeView';
 import {elements, renderLoader, clearLoader} from "./views/base";
@@ -74,7 +75,6 @@ const controlRecipe = async () => {
         try {
             // Get recipe data and parse ingredients
             await state.recipe.getRecipe();
-            console.log(state.recipe.ingredients);
             state.recipe.parseIngredients();
 
             // Calculate servings and time
@@ -104,5 +104,7 @@ elements.recipe.addEventListener('click', e => {
         state.recipe.updateServing('inc');
         recipeView.updateServingsIngredients(state.recipe);
     }
-    console.log(state.recipe);
 })
+
+window.l = new List();
+
