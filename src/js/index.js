d32521a1ecd63a6f8d07117dfcf0a0aee886cea4
vannnyle@gmail.com <vannnyle@gmail.com>
2020-07-29 15:37:42 +0300

    Build the shopping list view

--- src/js/index.js
+++ src/js/index.js
@@ -3,6 +3,7 @@ import Recipe from "./models/Recipe";
 import List from "./models/List";
 import * as searchView from './views/searchView';
 import * as recipeView from './views/recipeView';
+import * as listView from './views/listView';
 import {elements, renderLoader, clearLoader} from "./views/base";
 /*
 Global state of the app:
@@ -107,4 +108,5 @@ elements.recipe.addEventListener('click', e => {
 })
 
 window.l = new List();
+
 
